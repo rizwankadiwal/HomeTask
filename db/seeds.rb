7cad9b3779beb89e281faf6e5813af430a76c77f
Rizwan Kadiwal <rizwan.kadiwal@yahoo.ca>
2018-04-16 23:20:57 -0500

    All exercise complete and final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,17 @@
 
 
 Task.destroy_all
-puts "Deleted all Task data"
+puts "Deleted all previous Task & Comment data"
+
+18.times do
+  new_task = Task.create(task_name: Faker::Hacker.say_something_smart,
+                         user_name: Faker::Internet.user_name)
+  new_task.save
+  10.times do
+    new_comment = new_task.comments.create(content: Faker::Hacker.say_something_smart)
+    new_comment.save
+  end
+end
+
+puts "Seed Script Generated #{Task.count} tasks."
+puts "Seed Script Generated #{Comment.count} comments."
